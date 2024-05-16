443551dbb0efad72c0c65ce639c1bb89baa36daf
Mihail <mihal.pilipenko2002@mail.ru>
2024-05-16 18:19:39 +0300

    Adding admin

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 let express;
 let cors;
 let dbConfig;
@@ -11,17 +12,44 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 dbConfig = require('./config/db.config');
-db = require('./models/index');
+db = require('./models');
+const User = db.user;
 
 db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
     console.log("MongoDB Connected");
-}).catch(()=>{
-    console.log("Error. DB is not connected");
+    initial();
+}).catch((err)=>{
+    console.log("Error. DB is not connected:", err);
     process.exit();
 })
+
+
+function initial() {
+    User.estimatedDocumentCount((err, count) => {
+        if (!err)
+        {
+            console.log("Users count: ,",count)
+            if (count == 0)
+            {
+                new User({
+                    username: "Admin",
+                    nickname: "Muxac",
+                    email: "test@test.com",
+                    password: "test123",
+                }).save((err) => {
+                    if (err) {
+                        console.log("Admin not saved: ", err);
+                    } else {
+                        console.log("Admin saved");
+                    }
+                })
+            }
+        }
+    });
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
